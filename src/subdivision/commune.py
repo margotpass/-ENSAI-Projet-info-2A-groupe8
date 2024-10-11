dd8a3e5bea461f6c3b19457ac72a815ff48a582d
Margot Passard <margot.passard@eleve.ensai.fr>
2024-10-11 15:29:29 +0200

    commune

--- src/subdivision/commune.py
+++ src/subdivision/commune.py
@@ -1,29 +1,44 @@
-class Commune :
-    def __init__ (self, ID_CO, NOM_M, INSEE_COM, STATUT, INSEE_CAN, INSEE_ARR, INSEE_DEP, INSEE_REG, SIREN_EPCI, Polygons):
+class commune:
+    def __init__(self, ID_CO, NOM_M, INSEE_COM, STATUT, INSEE_CAN, INSEE_ARR,
+                 INSEE_DEP, INSEE_REG, SIREN_EPCI, Polygons):
+        
+        """
+        Initialise la classe Commune.
+
+        Paramètres:
+        -----------
+        ID_CO : str
+            identifiant de la commune
+        NOM_M : str
+            nom de la commune
+        INSEE
+        """
+
         if not isinstance(ID_CO, str):
             raise TypeError("L'ID doit être un str")
 
         if not isinstance(NOM_M, str):
             raise TypeError("Le nom doit être un str")
-        
+
         if not isinstance(INSEE_COM, str):
             raise TypeError("Le code INSEE de la commune doit être un str")
-        
+
         if not isinstance(STATUT, str):
             raise TypeError("Le statut doit être un str")
-        
+
         if not isinstance(INSEE_CAN, str):
             raise TypeError("Le code INSEE du canton doit être un str")
-        
+
         if not isinstance(INSEE_ARR, str):
-            raise TypeError("Le code INSEE de l'arrondissement doit être un str")
-        
+            raise TypeError("Le code INSEE de l'arrondissement doit être un"
+                            " str")
+
         if not isinstance(INSEE_DEP, str):
             raise TypeError("Le code INSEE du département doit être un str")
-        
+
         if not isinstance(INSEE_REG, str):
             raise TypeError("Le code INSEE de la région doit être un str")
-        
+
         if not isinstance(SIREN_EPCI, str):
             raise TypeError("Le code SIREN doit être un str")
 
