6c772cf4c61b8c6967aa31a1db004601c33c896d
maryemguizani <mariemguizani0@gmail.com>
2024-11-22 21:56:08 +0100

    maryem

--- app.py
+++ app.py
@@ -10,34 +10,47 @@ subdivision_service = SubdivisionService()
 
 
 @app.get("/recherche/{type_subdivision}/{id}")
-async def rechercher_information(type_subdivision: str, id: str,
-                                 annee: int = 2024,
-                                 insee_dep: str = None):
+async def rechercher_information(type_subdivision: str, id: str, annee: int = 2024, insee_dep: str = None):
     try:
-        subdision = subdivision_service.chercherSubdivisionParID(
-            type_subdivision, id, annee, insee_dep)
+        subdision = subdivision_service.chercherSubdivisionParID(type_subdivision, id, annee, insee_dep)
         return subdision
     except ValueError as e:
         raise HTTPException(status_code=404, detail=str(e))
 
 
 @app.get("/localisation/{type_subdivision}")
-async def localisation_point(type_subdivision: str, lat: float, long: float,
-                             annee: int = 2024):
+async def localisation_point(type_subdivision: str, lat: float,
+                             long: float, annee: int = 2024,
+                             typecoordonnees: str = "WGS84"):
     """
     Localise un point géographique (latitude, longitude) dans un niveau
     spécifié pour une année donnée.
     """
+    # Convertir les coordonnées si besoin
+    if typecoordonnees != "WGS84":
+        if typecoordonnees == "Lamb93":
+            lat, long = PointGeographique(lat, long, typecoordonnees).convertir_type_coordonnees()
+            typecoordonnees = "WGS84"  # Mise à jour du type après conversion
+        else:
+            raise HTTPException(
+                status_code=400,
+                detail=f"Type de coordonnées inconnu : {typecoordonnees}"
+            )
     # Créer un objet PointGeographique pour représenter les coordonnées
-    point = PointGeographique(latitude=lat, longitude=long)
+    point = PointGeographique(latitude=lat, longitude=long,
+                              typecoordonnees=typecoordonnees)
 
     # Utiliser le service pour localiser la subdivision contenant ce point
-    subdivision = LocalisationService().localiserPointDansSubdivision(point, type_subdivision, annee)
+    subdivision = LocalisationService().localiserPointDansSubdivision(
+     point,
+     type_subdivision,
+     annee
+    )
     if type_subdivision in ["Commune", "Departement", "Region", "EPCI"]:
         if subdivision:
             result = {
                 "code_insee": subdivision[0],
-                "type": type_subdivision,
+                "niveau": type_subdivision,
                 "nom": subdivision[1].get("nom"),
                 "annee": annee,
                 "coordonnees": {"latitude": lat, "longitude": long}
@@ -48,7 +61,7 @@ async def localisation_point(type_subdivision: str, lat: float, long: float,
             result = {
                 "code_insee": subdivision[0],
                 "insee_dep": subdivision[2],
-                "type": type_subdivision,
+                "niveau": type_subdivision,
                 "nom": subdivision[1].get("nom"),
                 "annee": annee,
                 "coordonnees": {"latitude": lat, "longitude": long}
@@ -56,8 +69,8 @@ async def localisation_point(type_subdivision: str, lat: float, long: float,
             return result
 
     else:
-        raise HTTPException(status_code=404, detail="Point non localisé dans"
-                            " une subdivision.")
+        raise HTTPException(status_code=404,
+                            detail="Point non localisé dans une subdivision.")
 
 
 @app.post("/localisation-liste/")
@@ -65,13 +78,12 @@ async def localisation_liste_points(file: UploadFile = File(...),
                                     type_subdivision: str = "Commune",
                                     annee: int = 2024):
     """
-    Localise un ensemble de points fournis dans un fichier CSV pour un niveau"
-    "géographique spécifié et une année donnée.
+    Localise un ensemble de points fournis dans un fichier CSV pour un niveau
+    géographique spécifié et une année donnée.
     """
     # Vérifier le format du fichier
     if not file.filename.endswith(".csv"):
-        raise HTTPException(status_code=400, detail="Le fichier doit être au"
-                            " format .csv")
+        raise HTTPException(status_code=400, detail="Le fichier doit être au format .csv")
 
     file_path = "localisation_resultats.csv"
 
@@ -81,38 +93,34 @@ async def localisation_liste_points(file: UploadFile = File(...),
         input_file.write(contents)
 
     # Traitement des points
-    with open("input.csv", mode="r") as csvfile, open(file_path,
-                                                      mode="w",
-                                                      newline="") as csv_output:
+    with open("input.csv", mode="r") as csvfile, open(file_path, mode="w", newline="") as csv_output:
         reader = csv.DictReader(csvfile)
         writer = csv.writer(csv_output)
-        writer.writerow(["Latitude", "Longitude", "Subdivision", "Code INSEE",
-                         "Annee"])  # En-têtes du fichier
+        writer.writerow(["Latitude", "Longitude", "Code INSEE", "niveau","Nom", "Annee"])  # En-têtes du fichier
 
         for row in reader:
             lat = float(row["latitude"])
             long = float(row["longitude"])
             point = PointGeographique(latitude=lat, longitude=long)
 
-            # Utiliser le service pour localiser chaque point dans la
-            # subdivision demandée
+            # Utiliser le service pour localiser chaque point dans la subdivision demandée
             subdivision = LocalisationService().localiserPointDansSubdivision(point, type_subdivision, annee)
 
             if subdivision:
-                writer.writerow([lat, long, subdivision, annee])
+                code_subdivision = subdivision[0]
+                niveau = type_subdivision
+                nom_subdivision = subdivision[1]["nom"]
+                writer.writerow([lat, long, code_subdivision, niveau, nom_subdivision, annee])
             else:
                 writer.writerow([lat, long, "Non localisé", "", annee])
 
-    return FileResponse(path=file_path, filename="localisation_resultats.csv",
-                        media_type="text/csv")
+    return FileResponse(path=file_path, filename="localisation_resultats.csv", media_type="text/csv")
 
 
 @app.get("/zonage/{type_subdivision}/{id}")
-async def obtenir_zonage(type_subdivision: str, id: str, annee: int = 2024,
-                         dep: str = None):
+async def obtenir_zonage(type_subdivision: str, id: str, annee: int = 2024, dep: str = None):
     """
-    Retourne le zonage complet d'une subdivision, y compris ses subdivisions
-    supérieures.
+    Retourne le zonage complet d'une subdivision, y compris ses subdivisions supérieures.
     """
     try:
         zonage = subdivision_service.zonage(type_subdivision, id, annee, dep)
